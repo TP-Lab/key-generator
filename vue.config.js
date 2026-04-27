05eeddd394d7b869595e2c5c8c81e406dbc3f0a1
ycg <122015474@qq.com>
2026-04-27 14:21:34 +0800

    uedate command

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
-  publicPath: '/',
-  // publicPath: '/key-generator',
+  publicPath: process.env.VUE_APP_ENV !== 'production' ? '/key-generator' : '/',
   devServer: {
     open: true,
     port: 12306,
